cef20cd4534b3e4cd36e11691baecfe322831769
Fybea <alexzey.198@gmail.com>
2024-11-13 02:44:35 +0300

    some refactor

--- src/test/java/com/example/telegram_bot_message_holiday_day_is_today/TelegramBotMessageHolidayDayIsTodayApplicationTests.java
+++ src/test/java/com/example/telegram_bot_message_holiday_day_is_today/TelegramBotMessageHolidayDayIsTodayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TelegramBotMessageHolidayDayIsTodayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
